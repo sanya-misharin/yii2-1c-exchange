3b5c9a83f03528aa9905f5f01f758e3796070a2e
Sanya Misharin <sanyamisharin.dev@gmail.com>
2025-09-25 12:37:49 +0300

    Fix: Remove html_entity_decode in bom behavior

--- behaviors/BomBehavior.php
+++ behaviors/BomBehavior.php
@@ -2,6 +2,7 @@
 
 namespace carono\exchange1c\behaviors;
 
+use http\Encoding\Stream;
 use Yii;
 use yii\base\ActionFilter;
 use yii\web\Response;
@@ -11,7 +12,8 @@ class BomBehavior extends ActionFilter
     public function beforeAction($action)
     {
         $response = Yii::$app->getResponse();
-        $response->on(Response::EVENT_AFTER_PREPARE, [$this, 'addBomToResponse'],
+        $response->on(Response::EVENT_AFTER_PREPARE,
+            [$this, 'addBomToResponse'],
             ['module' => $action->controller->module]);
 
         return parent::beforeAction($action);
@@ -22,19 +24,13 @@ class BomBehavior extends ActionFilter
         $response = $event->sender;
 
         if (is_string($response->content)) {
-            $xml = chr(0xEF) . chr(0xBB) . chr(0xBF) . $response->content;
-
-            if (isset($event->data['module']) && $event->data['module']->encodeQueryResponse) {
-                $xml = html_entity_decode($xml, ENT_QUOTES | ENT_HTML5 | ENT_COMPAT, 'UTF-8');
-            }
-
-            $response->content = $this->minifyXml($xml);
+            $response->content = $this->minifyXml(chr(0xEF) . chr(0xBB) . chr(0xBF) . $response->content);
         }
 
         $response->headers->set('Content-Type', 'text/xml; charset=UTF-8');
     }
 
-    private function minifyXml(string $xmlString): string
+    private function minifyXml(string $xmlString)
     {
         $xmlString = preg_replace('/>\s+</', '><', $xmlString);
 
